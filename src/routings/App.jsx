e3774e81421b0acca4d15fe1ec1be7e8b0da90be
edm20627 <eng00627@gmail.com>
2022-07-11 02:15:59 +0900

    お気に入り動画一覧ページの作成

--- src/routings/App.jsx
+++ src/routings/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import TopPage from '~/components/pages/TopPage';
 import PlayerPage from '~/components/pages/PlayerPage';
+import FavoritePage from '~/components/pages/FavoritePage';
 import NotFoundPage from '~/components/pages/NotFoundPage';
 
 const AppRouting = () => (
@@ -10,6 +10,7 @@ const AppRouting = () => (
     <Switch>
       <Route path="/" exact title="トップ" component={TopPage} />
       <Route path="/play/:videoId" title="動画再生" component={PlayerPage} />
+      <Route path="/favorites" title="お気に入り" component={FavoritePage} />
       <Route path="*" component={NotFoundPage} />
     </Switch>
   </BrowserRouter>
